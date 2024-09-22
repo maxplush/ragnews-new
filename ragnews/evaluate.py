821b3a11d8789b6e560310577ae8088d3f319889
maxplush <maxabeplush@gmail.com>
2024-09-22 14:13:27 -0700

    updated in class exercise

--- ragnews/evaluate.py
+++ ragnews/evaluate.py
@@ -2,8 +2,19 @@
 This file is for evaluating the quality of our RAG system 
 using the Hairy Trumpet tool/dataset.
 '''
+import sys
+sys.path.append('/Users/maxplush/Documents/ragnews-new')
 
 import ragnews
 
 class RAGEvaluator:
-    pass
+    def predict(self, x):
+        '''
+        >>> model = RAGEvaluator()
+        >>> mdoel.predict('There no mask token here.')
+        []
+        '''
+        db = ragnews.ArticleDB()
+        textprompt = 'hellow world'
+        output = ragnews.rag(textprompt, db)
+        return output
